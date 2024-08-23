ca76cb720a359fbb1f64a3480e8338a740e7260f
suragaryen <jennet967@gmail.com>
2024-08-23 13:39:40 +0900

    강한 결합을 느슨한 결합으로

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -12,10 +12,11 @@ import java.util.List;
 @RequestMapping("/api")
 public class MemoController {
 
+
     private final MemoService memoService;
 
-    public MemoController(JdbcTemplate jdbcTemplate) {
-        this.memoService = new MemoService(jdbcTemplate);
+    public MemoController(MemoService memoService) {
+        this.memoService = memoService;
     }
 
     @PostMapping("/memos")
